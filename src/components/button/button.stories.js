5d1fb44d4b4b5a47790947ae70481db25c7878e8
JonasImm <jonas.imm@gmail.com>
2020-07-28 16:19:30 +0200

    Add function to button

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,6 +1,26 @@
 import "./button.css";
-import button from "./button.html";
-
+/* import button from "./button.html";
+ */
 export default { title: "Button" };
 
-export const addToCart = () => button;
+function addButton(buttonText) {
+  let button = document.createElement("button");
+  button.classList.add("btn");
+  const text = document.createTextNode(buttonText);
+  button.append(text);
+  button.addEventListener("click", () => {
+    let anotherButton = addButton(buttonText);
+    button.parentElement.append(anotherButton);
+  });
+  button.addEventListener("hover", () => {
+    let anotherButton = addButton(buttonText);
+    button.parentElement.remove(anotherButton);
+  });
+  return button;
+}
+
+export const addToCart = () => {
+  let button = addButton("Hello there");
+
+  return button;
+};
